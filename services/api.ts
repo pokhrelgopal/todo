a353a01cc4fb2f0d4ffabb663d6f2b209a47069c
pokhrelgopal <pokhrelgopal27@gmail.com>
2024-01-27 13:17:33 +0545

    todos with pagination

---
+++ services/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const getTodos = async (page: any) => {
+  const response = await axios.get(`http://127.0.0.1:8000/todo/?${page}`);
+  return response.data;
+};
+
+export const getTodoViaEndpoint = async (endpoint: any) => {
+  const response = await axios.get(endpoint);
+  return response.data;
+};
+
+export const createTodo = async (data: any) => {
+  const response = await axios.post(`http://127.0.0.1:8000/todo/`, data);
+  return response.data;
+};
+
+export const deleteTodo = async (id: any) => {
+  const response = await axios.delete(`http://127.0.0.1:8000/todo/${id}/`);
+  return response.data;
+};
